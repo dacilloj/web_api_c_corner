9affa5291749528be0becef36f39368bdfd86d52
dacillo <johncarlodacillo123@gmail.com>
2022-12-05 17:54:18 +0000

    nullable values

--- Domain/Models/Address.cs
+++ Domain/Models/Address.cs
@@ -17,21 +17,21 @@ namespace Domain.Models
         {
             get;
             set;
-        }
+        } = "";
         public string City
         {
             get;
             set;
-        }
+        } = "";
         public string State
         {
             get;
             set;
-        }
+        } = "";
         public string ZipCode
         {
             get;
             set;
-        }
+        } = "";
     }
 }
